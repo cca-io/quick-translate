76af46fd4b7baf940876bbdffa0a43e200cad384
Florian Hammerschmidt <fh@cca.io>
2021-10-06 18:39:02 +0200

    Switch back to react for working fast refresh

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  resolve: {
-    alias: {
-      react: "preact/compat",
-      "react-dom": "preact/compat",
-    },
-  },
 });
