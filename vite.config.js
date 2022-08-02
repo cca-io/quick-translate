cccd3a007b058b93ba1b78b2a861b6b36c58e362
Florian Hammerschmidt <fh@cca.io>
2022-08-02 23:40:04 +0200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,44 @@
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
 export default {
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        "icon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Quick Translate",
+        short_name: "QT",
+        description: "Translate multiple languages at once in a spreadsheet",
+        theme_color: "#fddf19",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 };
