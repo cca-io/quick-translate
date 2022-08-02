76c6e847b09168f61169b353cbfbe8e1803759fc
Florian Hammerschmidt <fh@cca.io>
2022-08-02 18:59:01 +0200

    Upgrade all the things

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-});
+/**
+ * https://vitejs.dev/config/
+ * @type { import('vite').UserConfig }
+ */
+export default {
+  plugins: [react()],
+};
